82bbd5539bac327f5c208b8f74d858cf835f9706
skafula <79100340+skafula@users.noreply.github.com>
2023-07-14 19:00:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Quiz_App;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Question q1 = new Question();
+        Question q2 = new Question("Is hamburger an italian food?");
+        Question q3 = new Question("What's the capital city of Hungary?", "Debrecen", "Budapest", "Bucharest", "Bangkok", 'B');
+        Question q4 = new Question("Every Dr. is a doctor?") { optionA = "yes", optionB = "no", optionC = "maybe", optionD = "impossible" };
+
+        Console.WriteLine("q1 (false): " + q1.AreOptionsValid());
+        Console.WriteLine("q2 (false): " + q2.AreOptionsValid());
+        Console.WriteLine("q3 (true): " + q3.AreOptionsValid());
+        Console.WriteLine("q4 (true): " + q4.AreOptionsValid());
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_App
+{
+    internal class Question
+    {
+        public string questionText;
+        public string optionA;
+        public string optionB;
+        public string optionC;
+        public string optionD;
+        public char correctAnswerLetter;
+
+        static char defaultCorrectAnswerLetter;
+
+        public Question()
+        {
+            correctAnswerLetter = defaultCorrectAnswerLetter;
+        }
+
+        public Question(string questionText)
+        {
+            this.questionText = questionText;
+            correctAnswerLetter = defaultCorrectAnswerLetter;
+        }
+
+        public Question(string questionText, string optionA, string optionB, string optionC, string optionD, char correctAnswerLetter)
+        {
+            this.questionText = questionText;
+            this.optionA = optionA;
+            this.optionB = optionB;
+            this.optionC = optionC;
+            this.optionD = optionD;
+            this.correctAnswerLetter = correctAnswerLetter;
+        }
+
+        static Question()
+        {
+            defaultCorrectAnswerLetter = 'X';
+        }
+
+        public bool AreOptionsValid()
+        {
+            int i = 0;
+            i += (optionA != null) ? 1 : 0;
+            i += (optionB != null) ? 1 : 0;
+            i += (optionC != null) ? 1 : 0;
+            i += (optionD != null) ? 1 : 0;
+
+            return i > 1;
+        }
+    }
+}
